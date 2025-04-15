cc704bebde357eaf87099760ac1e636f7a1e981b
Jeremiah <oncwnuDRWy40riz0VMtaccflbQ-U@git.weixin.qq.com>
2025-04-15 18:50:26 -0400

    new logo

--- components/header.tsx
+++ components/header.tsx
@@ -33,7 +33,7 @@ export function Header() {
           <span className="flex items-center">
             <span className="shoot-text-left">Jeremiah</span>
             <Image
-              src="/logo4.png"
+              src="/logo6.png"
               alt="Logo"
               width={54}
               height={54}
